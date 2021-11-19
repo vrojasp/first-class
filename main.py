6848d8f41b2050497b4255c22b0958f5c25b4752
vrojas <experthomehop@gmai.com>
2021-11-19 01:44:10 -0400

    app

---
+++ main.py
@@ -0,0 +1,16 @@
+#from flask import Flask
+
+#app = Flask (__name__)
+
+#    @app.route('/')
+#    def index():
+#        return 'HELLO WORLD'
+
+#if __name__ == '__main__':
+#    app.run (port = 3000, debug = True)
+#DESDE LA CASA HACIENDO PRUEBAS
+
+
+a= 4+4
+b= 5*5+2+5
+print(b+a)
